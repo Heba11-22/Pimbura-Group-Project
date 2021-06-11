f40c1e300a18c46ec0295b85ccffdbc4583080a2
Savannah Rice <savannah.rice@gmail.com>
2021-06-11 11:23:47 -0600

    added links to footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -21,20 +21,20 @@ const Footer = () => {
                         <a href="https://www.linkedin.com/in/heba-e-3091261b5/"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
-                <div className='footer-links'><a href="">
+                <div className='footer-links'><a href="https://mimike.github.io/">
                     <p>Mimi Ke</p>
                     </a>
                     <div className='footer-icons'>
-                        <a href=""><i class="fab fa-github"></i></a>
-                        <a href=""><i class="fab fa-linkedin"></i></a>
+                        <a href="https://github.com/mimike"><i class="fab fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/mimi-ke-a7976031/"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
                 <div className='footer-links'><a href="">
                     <p>Marc Carlson</p>
                     </a>
                     <div className='footer-icons'>
-                        <a href=""><i class="fab fa-github"></i></a>
-                        <a href=""><i class="fab fa-linkedin"></i></a>
+                        <a href="https://github.com/Mcarlson30"><i class="fab fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/marc-carlson/"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
             {/* </ul> */}
